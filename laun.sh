c12cd981302181618e2ca9173bfa00df3ba3befa
mmomrk <mr.ak-47@rambler.ru>
2018-03-30 17:44:31 +0300

    test commit after git info fixed

--- laun.sh
+++ laun.sh
@@ -1,9 +1,14 @@
-if (( $# >= 6 )); then
+if (( $# >= 6 ));
+then
+
 ./plottr.py $* &&
 echo command generated &&
 chmod +x iplot.plt &&
 ./iplot.plt &&
 feh -F plot.png;
+
 else
+
 ./plottr.py -h;
+
 fi
